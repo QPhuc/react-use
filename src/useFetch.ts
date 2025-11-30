ee8f8467d6bb07d4f9b31bcfb72086cd4021bcc6
Phuc Tran <tqp1610@gmail.com>
2025-11-30 12:18:12 +0700

    feat: add useFetch hook for data fetching from a given URL

---
+++ src/useFetch.ts
@@ -0,0 +1,29 @@
+import { useEffect, useState } from "react";
+
+export default function useFetch(url: string) { 
+    const [data, setData] = useState(null);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState<Error | null>(null);
+    
+    useEffect(() => {
+        fetch(url)
+            .then((response) => {
+                if (!response.ok) {
+                    throw new Error("Network response was not ok");
+                }
+                return response.json();
+            })
+            .then((data) => {
+                setData(data);
+                setLoading(false);
+            })
+            .catch((error) => {
+                setError(error);
+                setLoading(false);
+            }).finally(() => {
+                setLoading(false);
+            });
+    }, [url]);
+    
+    return { data, loading, error };
+}
