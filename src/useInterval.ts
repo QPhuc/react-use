241624155974c427e5cc93d3919f36358e438287
Phuc Tran <tqp1610@gmail.com>
2025-12-02 18:49:13 +0700

    add

--- src/useInterval.ts
+++ src/useInterval.ts
@@ -1,5 +1,6 @@
 import { useEffect } from "react";
 
+// useInterval(() => { console.log("Hello"); }, 1000);
 export default function useInterval(callback: () => void, delay: number | null): void {
     useEffect(() => {
         if (delay !== null) {
--- src/useLazyLoad.ts
+++ src/useLazyLoad.ts
@@ -1,6 +1,9 @@
 import { lazy } from "react";
 
 // const Component = useLazyLoad("./MyComponent", "MyComponent");
+// Component: React.LazyExoticComponent<React.ComponentType<any>>
+// Dynamically imports a component from the specified path.
+// If nameExport is provided, it imports that specific export from the module.
 export default function useLazyLoad(path: string, nameExport: string) {
     return lazy(() => {
         const promise = import(path);
