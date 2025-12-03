3d6c4cb1da3c12506f31273d36e400102142356e
Phuc Tran <tqp1610@gmail.com>
2025-12-03 22:20:38 +0700

    add useHover

---
+++ src/useHover.ts
@@ -0,0 +1,33 @@
+import { cloneElement, useState } from "react";
+import { noop } from "./misc/util";
+
+export type Element = ((state: boolean) => React.ReactElement<any>) | React.ReactElement<any>;
+
+// const [element, isHovered] = useHover(element);
+// element: ReactElement
+// isHovered: boolean
+const useHover = (element: Element): [React.ReactElement<any>, boolean] => {
+    const [state, setState] = useState(false);
+
+    const onMouseEnter = (originalOnMouseEnter?: any) => (event: any) => {
+        (originalOnMouseEnter || noop)(event);
+        setState(true);
+    };
+    const onMouseLeave = (originalOnMouseLeave?: any) => (event: any) => {
+        (originalOnMouseLeave || noop)(event);
+        setState(false);
+    };
+
+    if (typeof element === 'function') {
+        element = element(state);
+    }
+
+    const el = cloneElement(element, {
+        onMouseEnter: onMouseEnter(element.props.onMouseEnter),
+        onMouseLeave: onMouseLeave(element.props.onMouseLeave),
+    });
+
+    return [el, state];
+};
+
+export default useHover;
